b0bf4f65e0892daafb4a57acbb0f3664a4ed62bf
Venteto <g0jtb5nre@relay.firefox.com>
2025-03-14 10:58:52 -0400

    add detail to the TS render demo

--- vto_core/urls.py
+++ vto_core/urls.py
@@ -3,5 +3,5 @@ from vto_core.views import TZList, DemoTestTSList
 
 urlpatterns = [
     path('core/iana-tz-list/', TZList.as_view(), name='tz_list'),
-    path('core/demo-ts-render/', DemoTestTSList.as_view(), name='demo_ts_render'),
+    path('core/ts-render-demo/', DemoTestTSList.as_view(), name='ts_render_demo'),
 ]
